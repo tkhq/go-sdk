3c17b2d23110603238ae922a0b47d6836f20edc3
Olivia Thet <olivia@turnkey.io>
2024-03-06 10:55:51 -0500

    add enclave encrypt pkg

---
+++ pkg/enclave_encrypt/go.mod
@@ -0,0 +1,23 @@
+module github.com/tkhq/go-sdk/pkg/enclave_encrypt
+
+go 1.19
+
+require (
+	github.com/cloudflare/circl v1.3.7
+	github.com/stretchr/testify v1.8.4
+)
+
+require (
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
+
+require (
+	github.com/btcsuite/btcutil v1.0.2
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ pkg/enclave_encrypt/server.go
@@ -0,0 +1,127 @@
+package enclave_encrypt
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+
+	"github.com/btcsuite/btcutil/base58"
+	"github.com/cloudflare/circl/kem"
+)
+
+type EnclaveEncryptServer struct {
+	enclaveAuthKey *ecdsa.PrivateKey
+	targetPrivate  kem.PrivateKey
+}
+
+type EnclaveEncryptServerRecv struct {
+	targetPrivate kem.PrivateKey
+}
+
+// This should be the quorum signing secret derived from the quorum
+// master seed.
+func NewEnclaveEncryptServer(enclaveAuthKey *ecdsa.PrivateKey) (EnclaveEncryptServer, error) {
+	_, targetPrivate, err := KemId.Scheme().GenerateKeyPair()
+	if err != nil {
+		return EnclaveEncryptServer{}, err
+	}
+
+	return EnclaveEncryptServer{
+		enclaveAuthKey,
+		targetPrivate,
+	}, nil
+}
+
+// Encrypt `plaintext` to the `clientTarget` key.
+func (s *EnclaveEncryptServer) Encrypt(clientTarget []byte, plaintext []byte) (*ServerSendMsg, error) {
+	clientTargetKem, err := KemId.Scheme().UnmarshalBinaryPublicKey(clientTarget[:])
+	if err != nil {
+		return nil, err
+	}
+	ciphertext, encappedPublic, err := encrypt(
+		&clientTargetKem,
+		plaintext,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	encappedPublicSignature, err := P256Sign(s.enclaveAuthKey, encappedPublic)
+	if err != nil {
+		return nil, err
+	}
+
+	encSig := Bytes(encappedPublicSignature)
+	enc := Bytes(encappedPublic)
+	ciph := Bytes(ciphertext)
+	return &ServerSendMsg{
+		EncappedPublic:          &enc,
+		EncappedPublicSignature: &encSig,
+		Ciphertext:              &ciph,
+	}, nil
+}
+
+// Return the servers encryption target key and a signature over it from
+// the quorum key.
+func (s *EnclaveEncryptServer) PublishTarget() (*ServerTargetMsg, error) {
+	targetPublic, err := s.targetPrivate.Public().MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+	t := Bytes(targetPublic)
+
+	targetPublicSignature, err := P256Sign(s.enclaveAuthKey, targetPublic)
+	if err != nil {
+		return nil, err
+	}
+	tSig := Bytes(targetPublicSignature)
+
+	return &ServerTargetMsg{
+		TargetPublic:          &t,
+		TargetPublicSignature: &tSig,
+	}, nil
+}
+
+// Get the server receiving type.
+func (s *EnclaveEncryptServer) IntoEnclaveServerRecv() EnclaveEncryptServerRecv {
+	return EnclaveEncryptServerRecv{
+		targetPrivate: s.targetPrivate,
+	}
+}
+
+// Relevant for usage with auth activities: Email Auth, Email Recovery.
+func (s *EnclaveEncryptServer) AuthEncrypt(clientTarget []byte, plaintext []byte) (string, error) {
+	clientTargetKem, err := KemId.Scheme().UnmarshalBinaryPublicKey(clientTarget[:])
+	if err != nil {
+		return "", err
+	}
+	ciphertext, encappedPublic, err := encrypt(
+		&clientTargetKem,
+		plaintext,
+	)
+	if err != nil {
+		return "", err
+	}
+
+	// FIXME: `elliptic.Unmarshal` is deprecated, but scm does not know how to replace it.
+	// nolint:staticcheck
+	x, y := elliptic.Unmarshal(elliptic.P256(), encappedPublic)
+
+	compressedEncappedPublic := elliptic.MarshalCompressed(elliptic.P256(), x, y)
+	payload := append(compressedEncappedPublic, ciphertext...)
+
+	checksum := checksum(payload)
+	payload = append(payload, checksum[:]...)
+
+	return base58.Encode(payload), nil
+}
+
+// Decrypt a message from a client that encrypted to this server instance
+// target key.
+// Relevant for usage with auth activities: Email Auth, Email Recovery.
+func (s *EnclaveEncryptServerRecv) Decrypt(msg ClientSendMsg) ([]byte, error) {
+	return decrypt(
+		*msg.EncappedPublic,
+		s.targetPrivate,
+		*msg.Ciphertext,
+	)
+}
