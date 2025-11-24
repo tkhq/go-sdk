224fa69f58b47f3ecc7135f7d8707f58f4d532d1
Zeke Mostov <zeke@turnkey.io>
2025-11-24 16:23:55 -0500

    fix eamil otp example

--- examples/email_otp/main.go
+++ examples/email_otp/main.go
@@ -72,14 +72,16 @@ func initClient() {
 
 func sendOTP() (string, error) {
 	otpType := "OTP_TYPE_EMAIL"
+	appName := "Go SDK Email OTP Example"
 
 	params := user_verification.NewInitOtpParams().WithBody(&models.InitOtpRequest{
 		TimestampMs:    util.RequestTimestamp(),
 		OrganizationID: util.StringPointer(parentOrgID),
 		Type:           (*string)(models.ActivityTypeInitOtp.Pointer()),
-		Parameters: &models.InitOtpIntent{
+		Parameters: &models.InitOtpIntentV2{
 			Contact: util.StringPointer(emailAddress),
 			OtpType: &otpType,
+			AppName: &appName,
 		},
 	})
 
